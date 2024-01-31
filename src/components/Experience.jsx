4459f4eea5e19a9275c7b5dc09ee57a9247adb51
MgrGracz <kamil.bandzwolek@gmail.com>
2024-01-31 16:45:53 +0100

    v005

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -10,14 +10,14 @@ const Experience = () => {
         position={[-5, 5, 5]} 
         castShadow 
         shadow-mapSize={1024} />
-      <group position={[0, -1, 0]}>
+      <group position={[-1, -5, -4]}>
         <Polak 
           position={[0, 0, 0]} 
         />
        </group>
       <mesh 
         rotation={[-0.5 * Math.PI, 0, 0]} 
-          position={[0, -1, 0]} 
+          position={[-1, -5, -4]} 
           receiveShadow>
         <planeBufferGeometry args={[10, 10, 1, 1]} />
         <shadowMaterial transparent opacity={0.2} />
