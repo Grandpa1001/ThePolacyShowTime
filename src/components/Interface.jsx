d6b4e63e6136930469d6646c62bf8c97361de16d
MgrGracz <kamil.bandzwolek@gmail.com>
2024-02-10 20:47:26 +0100

    v017
    Fix buttons

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -1,35 +1,62 @@
-import { Affix, Button, Stack, Image} from "@mantine/core";
+import React, { useState } from "react";
+import { Affix, Stack, Image, Button, Drawer } from "@mantine/core";
 import { useCharacterAnimations } from "../contexts/CharacterAnimations";
 
-
 const Interface = () => {
-    const { animations, animationIndex, setAnimationIndex } = 
-    useCharacterAnimations();
-    return (
-        <>
-    <Affix position={{ bottom: 50, right: 20 }}>
+  const { animations, animationIndex, setAnimationIndex } = useCharacterAnimations();
+  const [drawerOpened, setDrawerOpened] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setDrawerOpened(!drawerOpened);
+  };
+
+  const handleAnimationButtonClick = (index) => {
+    setAnimationIndex(index);
+    setDrawerOpened(false); // Schowaj Drawer po wybraniu animacji
+  };
+
+  return (
+    <>
+      <Affix position={{ bottom: 50, right: 20 }}>
         <Stack>
-        <a href="https://www.thepolacy.pl/" target="blank"><Image src="https://www.thepolacy.pl/_next/static/media/logo.34b813ed.svg"  /></a>
-        <div />
-        <a href="https://opensea.io/assets/ethereum/0x99903e8ec87b9987bd6289df8eff178d6e533561/1127" target="blank">ThePolak #1127</a>
+          <a href="https://www.thepolacy.pl/" target="blank">
+            <Image src="https://www.thepolacy.pl/_next/static/media/logo.34b813ed.svg" />
+          </a>
+          <div />
+          <a href="https://opensea.io/assets/ethereum/0x99903e8ec87b9987bd6289df8eff178d6e533561/1127" target="blank">
+            ThePolak #1127
+          </a>
 
+          {/* Przycisk otwierający Drawer */}
+          <Button size="md" color="gray" variant="light" onClick={handleDrawerToggle}>
+            Wybierz animacje
+          </Button>
+
+          {/* Drawer z buttonami */}
+          <Drawer
+            opened={drawerOpened}
+            onClose={() => setDrawerOpened(false)}
+            title="Animations"
+            position="bottom"
+            padding="md"
+          >
             {animations.map((animation, index) => (
-                        <Button 
-                        key={animation}
-                        size="md"
-                        color="gray"
-                        variant={index === animationIndex ? "filled" : "light"}
-                        onClick={() => setAnimationIndex(index)}
-                        >
-                            {animation}
-                            </Button>
-                    ))}
+              <Button
+                key={animation}
+                size="md"
+                color="gray"
+                variant={index === animationIndex ? "filled" : "light"}
+                onClick={() => handleAnimationButtonClick(index)}
+                fullWidth
+              >
+                {animation}
+              </Button>
+            ))}
+          </Drawer>
         </Stack>
-    </Affix>
-
+      </Affix>
     </>
-    )
-}
-
+  );
+};
 
 export default Interface;
