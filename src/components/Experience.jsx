a83bc3aa4dab6eccab27bac4d02c73948e3ce0c7
MgrGracz <kamil.bandzwolek@gmail.com>
2024-02-12 20:48:51 +0100

    v020

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,6 +1,7 @@
+import React from 'react';
 import { OrbitControls } from "@react-three/drei";
-import Polak from "./Polak.jsx"
 
+import Polak from "./Polak.jsx";
 
 const Experience = () => {
   return (
@@ -8,24 +9,25 @@ const Experience = () => {
       <OrbitControls />
       <ambientLight />
       <directionalLight 
-        position={[-5, 5, 5]} 
+        position={[5, 5, 5]}
+        intensity={1}
+        color="white"
         castShadow 
-        shadow-mapSize={1024} />
-      <group position={[-1, -5, -4]}>
-        <Polak 
-          position={[0, 0, 0]} 
-        />
-       </group>
+        shadow-mapSize={1024} 
+      />
+      <group position={[0, -8, -8]}>
+        <Polak position={[0, 0, 0]} />
+      </group>
       <mesh 
         rotation={[-0.5 * Math.PI, 0, 0]} 
-          position={[-1, -5, -8]} 
-          receiveShadow>
-
-        <shadowMaterial transparent opacity={0.2} />
+        position={[0, -5, 0]} 
+        receiveShadow
+      >
+        <planeGeometry attach="geometry" />
+        <meshStandardMaterial attach="material" opacity={0.7} />
       </mesh>
     </>
   );
 };
-
 
 export default Experience;
